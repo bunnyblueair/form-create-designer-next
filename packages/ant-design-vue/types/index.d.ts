3c4b86670893e9f6cc84146b47f19ecda0f89bd8
xaboy <xaboy2005@qq.com>
2026-05-07 18:25:24 +0800

    功能优化

--- packages/ant-design-vue/types/index.d.ts
+++ packages/ant-design-vue/types/index.d.ts
@@ -42,6 +42,24 @@ type extendRule = ((arg: { t: t }) => Rule[]) | {
     prepend?: boolean;
 };
 
+//field选择项
+type FieldItem = {
+    icon?: string;
+    field: string;
+    label: string;
+    disabled?: boolean;
+    selectable?: boolean;
+    //修改当前规则的必填,禁用和说明
+    update?: {
+        required?: Boolean;
+        disabled?: Boolean;
+        info?: string;
+        title?: string;
+        props?: Object;
+    };
+    children?: FieldItem[];
+};
+
 //可拖入的组件列表
 type AllowDrag = string[] | {
     //可拖入的菜单列表
@@ -92,6 +110,8 @@ export interface Config {
     fieldReadonly?: boolean;
     //配置name是否可以编辑
     nameReadonly?: boolean;
+    //field选择项,支持多级
+    fieldList?: FieldItem[];
     //隐藏拖拽操作按钮
     hiddenDragMenu?: boolean;
     //隐藏拖拽按钮
@@ -333,7 +353,7 @@ export type Handle = Array<{
     //按钮名称
     label: String;
     //回调函数
-    callback: Function;
+    handle: Function;
 }>
 
 //描述规则
