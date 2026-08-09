194dab835dc2021d9538a959b3b4b95bc580d373
BunnyBlue <bunnyblueair@gmail.com>
2026-08-09 11:57:03 +0800

    Revert "fix bug"
    This reverts commit a37ee0f5a206d037d0615d7f01a0a5983499d8eb.

--- packages/ant-design-vue-next/src/utils/form.js
+++ packages/ant-design-vue-next/src/utils/form.js
@@ -1,37 +1,11 @@
 import formCreate from '@form-create/antdv-next';
 import installAntdvComponents from '@form-create/antdv-next/auto-import';
 
-function installLocalAntdvComponents(targetForm) {
-    const app = {
-        component(name, component) {
-            if (component === undefined) {
-                return targetForm.component(name);
-            }
-            targetForm.component(name, component);
-            return app;
-        },
-        use(plugin, ...options) {
-            const install = plugin && (plugin.install || (typeof plugin === 'function' ? plugin : undefined));
-            if (install) {
-                install.call(plugin, app, ...options);
-            }
-            return app;
-        }
-    };
-
-    // 动态规则由 FormCreate 自己解析，组件必须进入它的局部组件表。
-    installAntdvComponents({
-        useApp(install) {
-            install(targetForm, app);
-        }
-    });
-}
-
 const viewForm = formCreate;
-installLocalAntdvComponents(viewForm);
+installAntdvComponents(viewForm);
 
 const designerForm = formCreate.factory();
-installLocalAntdvComponents(designerForm);
+installAntdvComponents(designerForm);
 
 export default viewForm;
 
